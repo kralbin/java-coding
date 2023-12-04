043715c96054df040f74e9558adc8b459cc5dfcc
kralbin <150432041+kralbin@users.noreply.github.com>
2023-12-04 14:25:13 +0530

    Add files via upload
    Argument/ Parameterised Constructor

---
+++ Demo1.java
@@ -0,0 +1,13 @@
+class StudDetails1{
+	int roll;
+	String name;
+	StudDetails1(int i, String n){		
+		roll = i;
+		name = n;
+	}
+}
+public class Demo1{
+	public static void main(String[] args){
+	StudDetails1 s1= new StudDetails1(1,"albin");
+	}
+}
